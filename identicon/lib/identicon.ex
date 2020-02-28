1995157b205270e435db22c7ce6754c7ce1491e2
SirNicholas <zozulyak.nick@gmail.com>
2020-02-28 18:33:15 +0200

    WIP struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,11 +1,30 @@
 defmodule Identicon do
   def main(string) do
+    IEx.configure(inspect: [charlists: :as_lists]) # TODO: figure out how to manage this in project configs
     string
     |> hash_string
+    |> pick_color
+    |> build_grid
   end
 
   def hash_string(string) do
-    :crypto.hash(:md5, string)
-    |> :binary.bin_to_list
+    hex = :crypto.hash(:md5, string) |> :binary.bin_to_list
+    %Identicon.Image{ hex: hex }
+  end
+
+  def pick_color(%Identicon.Image{ hex: [r, g, b | _tail] } = image) do
+    # Enum.take(image.hex, 3)
+    %Identicon.Image{ image | color: [r, g, b] }
+  end
+
+  def build_grid(%Identicon.Image{ hex: hex } = image) do
+    new_hex = hex
+    |> List.delete_at(-1)
+    |> Enum.chunk_every(3)
+    %Identicon.Image{ image | hex: new_hex }
+  end
+
+  def mirror_row(row) do
+
   end
 end
