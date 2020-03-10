1054ba23c0c27149cda4194900fcf94e1dc97744
SirNicholas <zozulyak.nick@gmail.com>
2020-03-10 12:51:47 +0200

    add filtering

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,10 +1,13 @@
 defmodule Identicon do
+  require Integer
+
   def main(string) do
     IEx.configure(inspect: [charlists: :as_lists]) # TODO: figure out how to manage this in project configs
     string
     |> hash_string
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
   def hash_string(string) do
@@ -12,19 +15,31 @@ defmodule Identicon do
     %Identicon.Image{ hex: hex }
   end
 
-  def pick_color(%Identicon.Image{ hex: [r, g, b | _tail] } = image) do
+  def pick_color(%Identicon.Image{ hex: [r, g, b | _] } = image) do
     # Enum.take(image.hex, 3)
     %Identicon.Image{ image | color: [r, g, b] }
   end
 
   def build_grid(%Identicon.Image{ hex: hex } = image) do
-    new_hex = hex
-    |> List.delete_at(-1)
-    |> Enum.chunk_every(3)
-    %Identicon.Image{ image | hex: new_hex }
+    grid =
+      hex
+      |> List.delete_at(-1)
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Identicon.Image{ image | grid: grid }
+  end
+
+  def filter_odd_squares(%Identicon.Image{ grid: grid } = image) do
+    new_grid = Enum.filter grid, fn({ value, _ }) ->
+      Integer.is_even(value)
+    end
+    %Identicon.Image{ image | grid: new_grid }
   end
 
   def mirror_row(row) do
-
+    [first, second | _] = row
+    row ++ [second, first]
   end
 end
