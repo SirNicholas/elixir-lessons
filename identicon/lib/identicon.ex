0754b5601198b6f1df2ec8b4dc776e856f1550e5
SirNicholas <zozulyak.nick@gmail.com>
2020-03-11 18:12:51 +0200

    finalize identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -8,6 +8,9 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(string)
   end
 
   def hash_string(string) do
@@ -16,8 +19,7 @@ defmodule Identicon do
   end
 
   def pick_color(%Identicon.Image{ hex: [r, g, b | _] } = image) do
-    # Enum.take(image.hex, 3)
-    %Identicon.Image{ image | color: [r, g, b] }
+    %Identicon.Image{ image | color: { r, g, b } }
   end
 
   def build_grid(%Identicon.Image{ hex: hex } = image) do
@@ -41,5 +43,29 @@ defmodule Identicon do
   def mirror_row(row) do
     [first, second | _] = row
     row ++ [second, first]
+  end
+
+  def build_pixel_map(%Identicon.Image{ grid: grid } = image) do
+    pixel_map = Enum.map grid, fn({ _, index }) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      top_left = { horizontal, vertical }
+      bottom_right = { horizontal + 50, vertical + 50 }
+      { top_left, bottom_right }
+    end
+    %Identicon.Image{ image | pixel_map: pixel_map }
+  end
+
+  def draw_image(%Identicon.Image{ color: color, pixel_map: pixel_map }) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+    Enum.each pixel_map, fn({ start, stop }) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+    :egd.render(image)
+  end
+
+  def save_image(image, string) do
+    File.write("#{string}.png", image)
   end
 end
